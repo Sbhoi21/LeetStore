126059e542f9cad5ede716914c9db4cc66694f4d
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2025-08-24 02:24:52 +0530

    Time: 7 ms (71.3%), Space: 44.4 MB (76.12%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,24 +1,17 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-                int n = nums.length;
-        int[] result = new int[n];
-        int[] count = new int[101]; // Since nums[i] is in the range [0, 100]
+          int repeat;
+        int[] count=new int[nums.length];
+        for (int i=0;i<nums.length;i++){
+            repeat=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    repeat++;
 
-        // Count occurrences of each number
-        for (int num : nums) {
-            count[num]++;
+                }
+            }
+            count[i]=repeat;
         }
-
-        // Calculate the prefix sum to find how many numbers are smaller
-        for (int i = 1; i < count.length; i++) {
-            count[i] += count[i - 1];
-        }
-
-        // Fill the result array
-        for (int i = 0; i < n; i++) {
-            result[i] = nums[i] == 0 ? 0 : count[nums[i] - 1];
-        }
-
-        return result;
+        return count;
     }
 }
