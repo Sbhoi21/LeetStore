85c60c335f984d3acde97e433453567386f754c4
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2025-08-31 20:17:33 +0530

    Time: 1 ms (99.83%), Space: 45.5 MB (71.71%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,42 +1,22 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-         int n = nums.length;
-        int xorSum = 0;
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1];
+        int dup = -1, miss = -1;
 
-        // Step 1: Compute the XOR of all elements in the array and numbers from 1 to n
-        for (int num : nums) {
-            xorSum ^= num;
+        for(int num: nums){
+            if(seen[num]){
+                dup = num;
+            }
+            seen[num] = true;
         }
-        for (int i = 1; i <= n; i++) {
-            xorSum ^= i;
-        }
 
-        // Step 2: Find a set bit (rightmost set bit)
-        int setBit = xorSum & -xorSum;
-
-        // Step 3: Divide numbers into two groups and compute XOR for each group
-        int xorGroup1 = 0, xorGroup2 = 0;
-        for (int num : nums) {
-            if ((num & setBit) != 0) {
-                xorGroup1 ^= num;
-            } else {
-                xorGroup2 ^= num;
+        for(int i = 1; i <= n; i++){
+            if(!seen[i]){
+                miss = i;
+                break;
             }
         }
-        for (int i = 1; i <= n; i++) {
-            if ((i & setBit) != 0) {
-                xorGroup1 ^= i;
-            } else {
-                xorGroup2 ^= i;
-            }
-        }
-
-        // Step 4: Determine which is the duplicate and which is the missing number
-        for (int num : nums) {
-            if (num == xorGroup1) {
-                return new int[]{xorGroup1, xorGroup2};
-            }
-        }
-        return new int[]{xorGroup2, xorGroup1};
+        return new int[]{dup, miss};
     }
 }
