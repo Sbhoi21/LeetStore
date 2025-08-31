112c21cebcc6f433ffe13e372b212bebe3e417fd
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2025-08-31 23:03:51 +0530

    Time: 2 ms (92.71%), Space: 43.6 MB (13.23%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-  java.util.Set<Integer> set = new java.util.HashSet<>();
+  Set<Integer> set = new HashSet<>();
         for (int num : arr) {
             if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
                 return true;
