7d238d57f3d155694a63cdfcd11bed871de01316
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2025-10-15 14:25:41 +0530

    Time: 1 ms (40.4%), Space: 41.6 MB (45.91%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.java
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.java
@@ -1,18 +1,13 @@
 class Solution {
     public String sortSentence(String s) {
-             String[] array = s.split(" ");
-		String[] result = new String[array.length];
+        String[] words = s.split(" ");
+        String[] sortedWords = new String[words.length];
 
-		for (String word : array) {
-			int i = Integer.parseInt(String.valueOf(word.charAt(word.length() - 1)));
-			
-			result[i - 1] = word.substring(0, word.length() - 1);
-		}
+        for (String word : words) {
+            int index = Character.getNumericValue(word.charAt(word.length() - 1)) - 1;
+            sortedWords[index] = word.substring(0, word.length() - 1);
+        }
 
-		StringBuilder s1 = new StringBuilder();
-		for (String word: result) {
-			s1.append(word + " ");
-		}
-		return s1.toString().substring(0 , s1.length()-1);
+        return String.join(" ", sortedWords);
     }
 }
