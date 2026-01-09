e4c6eb4e5a954540e350f4bd0c86e487ddb60dd7
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2026-01-09 19:57:56 +0530

    Time: 0 ms (100%), Space: 43.7 MB (33.06%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode node) {
+        if (node == null) {
+            return new Result(0, null);
+        }
+
+        Result left = dfs(node.left);
+        Result right = dfs(node.right);
+
+        if (left.depth > right.depth) {
+            return new Result(left.depth + 1, left.node);
+        } else if (right.depth > left.depth) {
+            return new Result(right.depth + 1, right.node);
+        } else {
+            // equal depth → this node is the LCA of deepest nodes
+            return new Result(left.depth + 1, node);
+        }
+    }
+
+    private static class Result {
+        int depth;
+        TreeNode node;
+
+        Result(int depth, TreeNode node) {
+            this.depth = depth;
+            this.node = node;
+        }
+    }
+}
