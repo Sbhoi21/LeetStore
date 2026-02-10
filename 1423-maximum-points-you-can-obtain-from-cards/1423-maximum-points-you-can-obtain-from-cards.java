9bc841359f44364dc17024536a6fb408be8e26bb
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2026-02-10 20:51:14 +0530

    Time: 2 ms (99.59%), Space: 61.8 MB (69.88%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -7,10 +7,9 @@ class Solution {
         }
         if (k >= n) return sum;
         int maxSum = sum;
-        while (k != 0) {
+        for ( ; k != 0; k--,n--) {
             sum += (cardPoints[n-1] - cardPoints[k-1]);
             maxSum = Math.max(maxSum, sum);
-            k--; n--;
         }
         return maxSum;
     }
