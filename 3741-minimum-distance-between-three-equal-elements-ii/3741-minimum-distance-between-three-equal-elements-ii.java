11c3c4aa8ba507235965e72e27fc646974e15c26
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2026-04-12 02:01:33 +0530

    Time: 43 ms (93.42%), Space: 162.4 MB (93.42%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -2,23 +2,19 @@ class Solution {
     public int minimumDistance(int[] nums) {
         int n = nums.length;
         int[][] last = new int[n + 1][2];
-        for (int i = 0; i <= n; i++) {
-            last[i][0] = -1;
-            last[i][1] = -1;
-        }
-
+        
         int ans = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             int val = nums[i];
 
-            if (last[val][0] != -1) {
-                ans = Math.min(ans, 2 * (i - last[val][0]));
+            if (last[val][0] != 0) {
+                ans = Math.min(ans, 2 * (i - last[val][0] +1));
             }
 
             // shift last two indices
             last[val][0] = last[val][1];
-            last[val][1] = i;
+            last[val][1] = i+1;
         }
 
         return ans == Integer.MAX_VALUE ? -1 : ans;
