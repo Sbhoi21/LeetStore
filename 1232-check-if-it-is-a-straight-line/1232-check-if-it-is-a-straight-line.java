25eb892d6509e3e9151086b601490e0222e3f8fb
Sbhoi21 <134804233+Sbhoi21@users.noreply.github.com>
2026-06-26 20:47:43 +0530

    Time: 0 ms (100%), Space: 45 MB (61.09%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-        float slope = (coordinates[1][0] - coordinates[0][0]) == 0 ? 15000
-                : (float) (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
-        System.out.println(slope);
+        int dx = (coordinates[1][0] - coordinates[0][0]);
+        int dy = (coordinates[1][1] - coordinates[0][1]);
+
         for (int i = 2; i < coordinates.length; i++) {
-            float tempSlope = (coordinates[i][0] - coordinates[i - 1][0]) == 0 ? 15000
-                    : (float) (coordinates[i][1] - coordinates[i - 1][1]) / (coordinates[i][0] - coordinates[i - 1][0]);
-            System.out.println("tempSlope:  " + tempSlope);
-            if (tempSlope != slope) {
+            int dx1 = (coordinates[i][0] - coordinates[i - 1][0]);
+            int dy1 = (coordinates[i][1] - coordinates[i - 1][1]);
+            if ((dy * dx1) != (dy1 * dx)) {
                 return false;
             }
         }
