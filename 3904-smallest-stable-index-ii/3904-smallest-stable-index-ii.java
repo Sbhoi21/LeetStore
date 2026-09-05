0eb9b43cef1884137ed2653493f6746100357964
Shubham Bhoi <134804233+Sbhoi21@users.noreply.github.com>
2026-09-06 01:25:39 +0530

    Time: 3 ms (100%), Space: 133.2 MB (36.95%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,25 +1,44 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
 
+        // int n = nums.length;
+        // int[] min = new int[n];
+        // int[] max = new int[n];
+        // int minNo = nums[n - 1], maxNo = nums[0];
+        // for (int i = 0; i < n; i++) {
+        //     maxNo = Math.max(nums[i], maxNo);
+        //     max[i] = maxNo;
+        //     minNo = Math.min(nums[n - i - 1], minNo);
+        //     min[n - i - 1] = minNo;
+        // }
+
+        // for (int i = 0; i < n; i++) {
+
+        //     if (max[i] - min[i] <= k) {
+        //         return i;
+        //     }
+
+        // }
+
+        // return -1;
+
         int n = nums.length;
         int[] min = new int[n];
-        int[] max = new int[n];
         int minNo = nums[n - 1], maxNo = nums[0];
-        for (int i = 0; i < n; i++) {
-            maxNo = Math.max(nums[i], maxNo);
-            max[i] = maxNo;
-            minNo = Math.min(nums[n - i - 1], minNo);
-            min[n - i - 1] = minNo;
+        for (int i = n - 1; i >= 0; i--) {
+            minNo = Math.min(nums[i], minNo);
+            min[i] = minNo;
         }
 
         for (int i = 0; i < n; i++) {
-
-            if (max[i] - min[i] <= k) {
+            maxNo = Math.max(nums[i], maxNo);
+            if (maxNo - min[i] <= k) {
                 return i;
             }
 
         }
 
         return -1;
+
     }
 }
